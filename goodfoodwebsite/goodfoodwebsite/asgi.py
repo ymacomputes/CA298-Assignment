7bf9a37c399a9ae0c69c3a9c3da7b5e939be35b0
amy <amy.collins49@mail.dcu.ie>
2021-03-15 19:50:19 +0000

    Committing the website set up

--- goodfoodwebsite/goodfoodwebsite/asgi.py
+++ goodfoodwebsite/goodfoodwebsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ca298amyeoghan project.
+ASGI config for goodfoodwebsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ca298amyeoghan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodfoodwebsite.settings')
 
 application = get_asgi_application()
--- goodfoodwebsite/goodfoodwebsite/wsgi.py
+++ goodfoodwebsite/goodfoodwebsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ca298amyeoghan project.
+WSGI config for goodfoodwebsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ca298amyeoghan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodfoodwebsite.settings')
 
 application = get_wsgi_application()
