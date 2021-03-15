b983ab8ea559178ff70499fb2174e14975ebed30
amy <amy.collins49@mail.dcu.ie>
2021-03-15 21:29:08 +0000

    Committing the set up of the app & views

---
+++ goodfoodwebsite/goodfoodapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GoodfoodappConfig(AppConfig):
+    name = 'goodfoodapp'
---
+++ goodfoodwebsite/goodfoodapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    return HttpResponse("Hello from registration page")
