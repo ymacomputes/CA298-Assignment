0fc9ed85543608b8b604f9b9b45ce23051935f36
Eoghan <eoghan.scanlan6@mail.dcu.ie>
2021-03-16 22:15:58 +0000

    Added models to admin

--- goodfoodwebsite/goodfoodapp/views.py
+++ goodfoodwebsite/goodfoodapp/views.py
@@ -8,3 +8,7 @@ def index(request):
 
 def register(request):
     return HttpResponse("Hello from registration page")
+
+
+def all_products(request):
+    all_p =Product.objects.all() #Template not done, 18:50 in lab 3, frontend work
