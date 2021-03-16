99f8e2121575651fe503a247a37ecf548d2e8e6f
Eoghan <eoghan.scanlan6@mail.dcu.ie>
2021-03-16 13:09:45 +0000

    Added order model

--- goodfoodwebsite/goodfoodapp/models.py
+++ goodfoodwebsite/goodfoodapp/models.py
@@ -8,3 +8,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
