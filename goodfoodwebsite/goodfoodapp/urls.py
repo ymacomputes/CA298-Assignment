e6affd3d27a1fea1f1c4c557d58678cf9ac5be1d
Eoghan <eoghan.scanlan6@mail.dcu.ie>
2021-03-16 22:43:03 +0000

    Added product info to urls and views

--- goodfoodwebsite/goodfoodapp/urls.py
+++ goodfoodwebsite/goodfoodapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('registration/', views.register, name="register"),
+    path('allproducts', views.all_products, name="all_products"),
+    path('singleproduct/<int:prodid>', views.singleproduct, name="product_single"),
+    path('myform/', views.myform)
 ]
--- goodfoodwebsite/goodfoodapp/views.py
+++ goodfoodwebsite/goodfoodapp/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .models import Product
+from .forms import *
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 
 def index(request):
@@ -11,4 +15,12 @@ def register(request):
 
 
 def all_products(request):
-    all_p =Product.objects.all() #Template not done, 18:50 in lab 3, frontend work
+    all_p =Product.objects.all()
+    return render(request, 'all_products.html', {'products': all_p})
+
+
+def singleproduct(request, prodid):
+    prod = get_object_or_404(Product, pk=prodid)
+    return render(request, 'single_product.html', {'product':prod})
+
+def myform(request):
