3298db06eb7abfbd84260cbd545b3fbba89ef75d
Eoghan <eoghan.scanlan6@mail.dcu.ie>
2021-03-16 13:04:56 +0000

    Added first model

--- goodfoodwebsite/goodfoodapp/models.py
+++ goodfoodwebsite/goodfoodapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
